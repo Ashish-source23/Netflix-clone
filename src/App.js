60f783b47c4e99f67935a52c0daa62b50d4c7461
officialdiwakar223@gmail.com <officialdiwakar223@gmail.com>
2022-08-06 22:58:51 +0530

    Second Commit Banner Creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
+import './App.css'; 
 import React from 'react';
 import Row from './Row';
+import requests from './request';
+import "./Row.css";
+import Banner from './Banner';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hey There</h1>
-    <Row>title</Row>
+    <div className="app">
+    {/* {Navbar} */}
+
+    <Banner/>
+
+    <Row  title = "Trending" 
+          fetchUrl={requests.fetchTrending}
+          isLargeRow
+
+    />
+    <Row title = "NETFLIX Originals" fetchUrl = {requests.fetchNetflixOriginals}/>
+    <Row title = "Top Rated" fetchUrl={requests.fetchTopRated}/>
+    <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies}/>
+    <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+    <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+    <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+    <Row title = "Documentries" fetchUrl={requests.fetchDocumantaries}/>
+    
     </div>
   );
 }
